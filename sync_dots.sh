fccf800325d7dea6cb2508e37fa10f248961e2ba
Stefan Lachmann <slac@kth.se>
2023-06-14 21:46:56 +0200

    fixed various little bugs and in sync_dots.sh

--- sync_dots.sh
+++ sync_dots.sh
@@ -1,8 +1,22 @@
-#!/usr/local/bin/bash
+#!/bin/bash
+
+
+filename="./location.txt"
+line_number=1
+
+location=$(sed "${line_number}q;d" "$filename")
+echo "Line $line_number: $location"
+
 
 function sync() {
-	# cp -r "$HOME/$1" .
-	cp -r "$1" . # location
+    if [ "$2" == "folder" ]; then
+        local folder_path=${1%/}  # Remove trailing slash from $1
+         cp -r "$folder_path" "$location"
+        echo "copied folder: $1 to $location/$1"
+    else
+        cp "$1" "$location"
+        echo "copied file: $1 to $location/$1"
+    fi
 }
 
 # use to automate task of removing unneeded files like *.swap or whatever
@@ -11,25 +25,26 @@ function cleanup() {
  	sudo rm -rf "$PWD"/"$1"
 }
 
-
 # sync files
-dotbot_list_files="dotbots_files_list.txt"
+dotbot_list_files="./dotbots_files.txt"
 while IFS= read -r entry; do
 		echo "syncing file: $entry"
 		sync "$entry"
 done < "$dotbot_list_files"
 
 
+
 # sync Folders
-dotbot_list_files="dotbots_folders_list.txt"
+dotbot_list_folders="./dotbots_folders.txt"
 while IFS= read -r entry; do
-		echo "syncing folder: $entry"
-		sync "$entry"
-done < "$dotbot_list_files"
+    echo "Syncing folder: $entry"
+    sync "$entry" "folder"
+done < "$dotbot_list_folders"
 
 
+# function completed
     if hash lolcat 2>/dev/null; then
-        'sync completed' | lolcat
+        echo 'sync completed' | lolcat
     else
          'sync completed'
     fi
